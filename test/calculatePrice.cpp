ed9f04e0224fec41d006f2b4af731a58daa674d8
Peter-210 <leepeter1020@gmail.com>
2025-12-17 19:59:20 -0500

    update tests for ceiling rounded prices

--- test/calculatePrice.cpp
+++ test/calculatePrice.cpp
@@ -12,45 +12,45 @@ TEST(CalculatePrice, HoursOnly) {
 TEST(CalculatePrice, MinutesOnly) {
 	EXPECT_EQ(makerspace::calculatePrice(0, 1), 0.5);
 	EXPECT_EQ(makerspace::calculatePrice(0, 15), 0.5);
-	EXPECT_EQ(makerspace::calculatePrice(0, 22), 0.5);
+	EXPECT_EQ(makerspace::calculatePrice(0, 22), 1.0);
 	EXPECT_EQ(makerspace::calculatePrice(0, 23), 1.0);
 	EXPECT_EQ(makerspace::calculatePrice(0, 30), 1.0);
-	EXPECT_EQ(makerspace::calculatePrice(0, 37), 1.0);
+	EXPECT_EQ(makerspace::calculatePrice(0, 37), 1.5);
 	EXPECT_EQ(makerspace::calculatePrice(0, 38), 1.5);
-	EXPECT_EQ(makerspace::calculatePrice(0, 45), 1.5);
-	EXPECT_EQ(makerspace::calculatePrice(0, 52), 1.5);
+	EXPECT_EQ(makerspace::calculatePrice(0, 45), 2.0);
+	EXPECT_EQ(makerspace::calculatePrice(0, 52), 2.0);
 	EXPECT_EQ(makerspace::calculatePrice(0, 53), 2.0);
 	EXPECT_EQ(makerspace::calculatePrice(0, 59), 2.0);
 }
 
 TEST(CalculatePrice, OneHourAnyMinutes) {
-	EXPECT_EQ(makerspace::calculatePrice(1, 1), 2.0);
-	EXPECT_EQ(makerspace::calculatePrice(1, 7), 2.0);
+	EXPECT_EQ(makerspace::calculatePrice(1, 1), 2.5);
+	EXPECT_EQ(makerspace::calculatePrice(1, 7), 2.5);
 	EXPECT_EQ(makerspace::calculatePrice(1, 8), 2.5);
 	EXPECT_EQ(makerspace::calculatePrice(1, 15), 2.5);
-	EXPECT_EQ(makerspace::calculatePrice(1, 22), 2.5);
+	EXPECT_EQ(makerspace::calculatePrice(1, 22), 3.0);
 	EXPECT_EQ(makerspace::calculatePrice(1, 23), 3.0);
 	EXPECT_EQ(makerspace::calculatePrice(1, 30), 3.0);
-	EXPECT_EQ(makerspace::calculatePrice(1, 37), 3.0);
+	EXPECT_EQ(makerspace::calculatePrice(1, 37), 3.5);
 	EXPECT_EQ(makerspace::calculatePrice(1, 38), 3.5);
 	EXPECT_EQ(makerspace::calculatePrice(1, 45), 3.5);
-	EXPECT_EQ(makerspace::calculatePrice(1, 52), 3.5);
+	EXPECT_EQ(makerspace::calculatePrice(1, 52), 4.0);
 	EXPECT_EQ(makerspace::calculatePrice(1, 53), 4.0);
 	EXPECT_EQ(makerspace::calculatePrice(1, 59), 4.0);
 }
 
 TEST(CalculatePrice, MaxHoursAnyMinutes) {
-	EXPECT_EQ(makerspace::calculatePrice(3, 1), 6.0);
-	EXPECT_EQ(makerspace::calculatePrice(3, 7), 6.0);
+	EXPECT_EQ(makerspace::calculatePrice(3, 1), 6.5);
+	EXPECT_EQ(makerspace::calculatePrice(3, 7), 6.5);
 	EXPECT_EQ(makerspace::calculatePrice(3, 8), 6.5);
 	EXPECT_EQ(makerspace::calculatePrice(3, 15), 6.5);
-	EXPECT_EQ(makerspace::calculatePrice(3, 22), 6.5);
+	EXPECT_EQ(makerspace::calculatePrice(3, 22), 7.0);
 	EXPECT_EQ(makerspace::calculatePrice(3, 23), 7.0);
 	EXPECT_EQ(makerspace::calculatePrice(3, 30), 7.0);
-	EXPECT_EQ(makerspace::calculatePrice(3, 37), 7.0);
+	EXPECT_EQ(makerspace::calculatePrice(3, 37), 7.5);
 	EXPECT_EQ(makerspace::calculatePrice(3, 38), 7.5);
 	EXPECT_EQ(makerspace::calculatePrice(3, 45), 7.5);
-	EXPECT_EQ(makerspace::calculatePrice(3, 52), 7.5);
+	EXPECT_EQ(makerspace::calculatePrice(3, 52), 8.0);
 	EXPECT_EQ(makerspace::calculatePrice(3, 53), 8.0);
 	EXPECT_EQ(makerspace::calculatePrice(3, 59), 8.0);
 }
